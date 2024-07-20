3cb00435d13d5ee1f160c76cbc98a35d3a3c75e1
Rahul Harsh <rahulharsh017@gmail.com>
2024-07-21 03:15:10 +0530

    Update styles and add API integration to Dashboard component

--- src/components/Dashboard/TabsComponent/index.js
+++ src/components/Dashboard/TabsComponent/index.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React,{useState} from 'react';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { ThemeProvider, createTheme } from '@mui/material';
+import Grid from '../Grid';
+import './style.css';
 
-function TabComponent() {
+export default function TabComponent({coins}) {
+  const [value, setValue] = useState('Grid');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+
+  };
+
+  const theme = createTheme({
+    palette:{
+        primary:{
+            main: '#3a80e9'
+        },
+    }
+  })
+
+
   return (
-    <div>TabComponent</div>
-  )
+    <ThemeProvider theme={theme} >
+      <TabContext value={value}>
+          <TabList onChange={handleChange} variant='fullWidth'>
+            <Tab label="Grid" value="Grid" sx={{color: 'var(--white)'}} />
+            <Tab label="List" value="List" sx={{color: 'var(--white)'}} />
+          </TabList>
+        <TabPanel value="Grid">
+          <div className='grid-flex'>
+          {coins.map((coin,i) =>{
+                return (
+                    <Grid coin={coin} key={i} />  
+                )
+            })}
+          </div>
+        </TabPanel>
+        <TabPanel value="List">List</TabPanel>
+      </TabContext>
+    </ThemeProvider >
+  );
 }
-
-export default TabComponent
--- src/pages/DashBoard.js
+++ src/pages/DashBoard.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import Header from '../components/Common/Header'
 import TabComponent from '../components/Dashboard/TabsComponent'
+import axios from 'axios'
 
 function DashBoard() {
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1')
+    .then((response) => {
+      console.log(response)
+      setCoins(response.data)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  },)
   return (
     <div>
         <Header />
-        <TabComponent />
+        <TabComponent coins={coins} />
     </div>
   )
 }
